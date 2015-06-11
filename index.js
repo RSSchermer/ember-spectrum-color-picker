029d7a0e2535c5124a0a336e6d46623dc02e6d9a
R.S. Schermer <roland0507@gmail.com>
2015-06-12 00:43:58 +0200

    Updated included hook's super call to modern version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-spectrum-color-picker',
 
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
 
     app.import('bower_components/spectrum/spectrum.js');
     app.import('bower_components/spectrum/spectrum.css');
