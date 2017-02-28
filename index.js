8a38a3af17ffb87229318c251c527f4b78e356c6
Richard Viney <richard.viney@gmail.com>
2017-02-28 14:56:47 +1300

    Made asset path configurable

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
+var defaults = {
+  assetPath: 'bower_components/spectrum'
+};
+
 module.exports = {
   name: 'ember-spectrum-color-picker',
 
   included: function(app) {
     this._super.included(app);
 
+    var options = (app && app.project.config(app.env)['emberSpectrumColorPicker']) || {};
+
+    var assetPath = options.assetPath || defaults.assetPath;
+
     if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import('bower_components/spectrum/spectrum.js');
+      app.import(path.join(assetPath, 'spectrum.js'));
     }
 
-    app.import('bower_components/spectrum/spectrum.css');
+    app.import(path.join(assetPath, 'spectrum.css'));
   }
 };
