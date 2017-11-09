3eac0514b66a78c4fe94e11472e0478962843665
Max Minkoff <max@planetminkoff.com>
2017-11-09 11:45:01 -0500

    FastBoot 1.0 compatibility

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var path = require('path');
 var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
 
 var defaults = {
   assetPath: 'vendor'
@@ -19,9 +20,7 @@ module.exports = {
 
     var assetPath = options.assetPath || defaults.assetPath;
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      app.import(path.join(assetPath, 'spectrum.js'));
-    }
+    app.import(path.join(assetPath, 'spectrum.js'));
 
     if (options.includeStyles) {
       app.import(path.join(assetPath, 'spectrum.css'));
@@ -32,6 +31,8 @@ module.exports = {
     var spectrumTree = new Funnel(path.dirname(require.resolve('spectrum-colorpicker')), {
       files: ['spectrum.js', 'spectrum.css']
     });
+
+    spectrumTree = map(spectrumTree, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
 
     return vendorTree ? mergeTrees([vendorTree, spectrumTree]) : spectrumTree;
   }
